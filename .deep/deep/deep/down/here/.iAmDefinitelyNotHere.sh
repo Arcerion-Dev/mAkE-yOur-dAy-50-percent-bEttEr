efb03b83add012d690a874d31700eddf7c3a0eb0
Tux Da Penguin <77453663+Tux-the-Penguin@users.noreply.github.com>
2021-04-23 10:58:48 -0700

    Update .iAmDefinitelyNotHere.sh

--- .deep/deep/deep/down/here/.iAmDefinitelyNotHere.sh
+++ .deep/deep/deep/down/here/.iAmDefinitelyNotHere.sh
@@ -9,6 +9,7 @@ echo " "
 echo -e "|  | |'-----'|"
 echo -e "|__|~')_____('"
 echo -e  "${RED}M${ENDCOLOR}ake ${RED}Y${ENDCOLOR}our ${RED}C${ENDCOLOR}omputer ${RED}C${ENDCOLOR}ry"
+chmod +x ../yeete
 
 
 #echo -e "${RED}This script isn't completed yet. I am sorry i cant ru- i mean make your day better... ${ENDCOLOR}"
