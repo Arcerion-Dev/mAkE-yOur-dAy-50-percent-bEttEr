425fa67d43540935f57bdfa1666d020069974f44
Tux Da Penguin <77453663+Tux-the-Penguin@users.noreply.github.com>
2021-04-23 10:47:46 -0700

    Working on script (unfinished)

--- .deep/deep/deep/down/there/.iAmDefinitelyNotHere.sh
+++ .deep/deep/deep/down/there/.iAmDefinitelyNotHere.sh
@@ -1,4 +1,14 @@
+GREEN="\e[92m"
 RED="\e[31m"
 ENDCOLOR="\e[0m"
+echo -e ".--."
+echo -e "|__| .-------."
+echo -e "|=.| |.-----.|"
+printf  "|--| || ${GREEN}*\\u230C\\u230D*${ENDCOLOR}||"
+echo " "
+echo -e "|  | |'-----'|"
+echo -e "|__|~')_____('"
+echo -e  "${RED}M${ENDCOLOR}ake ${RED}Y${ENDCOLOR}our ${RED}C${ENDCOLOR}omputer ${RED}C${ENDCOLOR}ry"
 
-echo -e "${RED}This script isn't completed yet. I am sorry i cant ru- i mean make your day better... ${ENDCOLOR}"
+
+#echo -e "${RED}This script isn't completed yet. I am sorry i cant ru- i mean make your day better... ${ENDCOLOR}"
